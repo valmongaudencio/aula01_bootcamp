f2e2f22a852305eee9445e54c4c7d7186c3c8be5
Valmon <valmongaudencio@gmail.com>
2024-09-27 01:11:14 -0300

    Aprendendo a versionar meu código

---
+++ exemplo_01.py
@@ -0,0 +1,2 @@
+#Python
+print(int(input("Digite um número:")) + int(input("Digite um segundo número:")))
---
+++ exemplo_02.py
@@ -0,0 +1,10 @@
+a = input("Informe um valor: ")
+b = input("Informe um segundo valor: ")
+
+# Convertendo as entradas para números inteiros (ajuste para float se necessário)
+a = int(a)
+b = int(b)
+
+# Realizando a soma e imprimindo o resultado
+soma = a + b
+print("A soma é:", soma)
